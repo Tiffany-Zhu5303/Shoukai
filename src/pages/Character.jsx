d8874ade303a8b6d9310e6683615421cd5fc30f0
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2024-05-05 00:19:16 -0400

    anime recieved for characters

--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -12,27 +12,37 @@ const Character = () => {
         catch{(alert('Something went wrong with that query, try again'))};
     }
 
+    const getAnime = async(char) => {
+        let response = await fetch(`https://api.jikan.moe/v4/characters/${char.mal_id}/anime`);
+        let json = await response.json();
+        return json;
+    }
+
     const callAPI = async(query) =>{
         let response = await fetch(query);
         let jsonOne = await response.json();
         response = await fetch(query);
         let jsonTwo = await response.json();
         console.log(jsonOne.data);
-        setCharacterOne(jsonOne.data);
-        setCharacterTwo(jsonTwo.data);
+        let animesOne = await getAnime(jsonOne.data);
+        let animesTwo = await getAnime(jsonTwo.data);
+        setCharacterOne({...jsonOne.data, animes: animesOne});
+        setCharacterTwo({...jsonTwo.data, animes: animesTwo});
     }
 
     return (
         <div className="character-page">
             {characterOne && characterTwo ? <h1>{characterOne.name} or {characterTwo.name}?</h1> : null}
+            {console.log(characterOne, characterTwo)}
             {characterOne && characterTwo ? <div className="characters">
                 <CharCard name={characterOne.name} name_kanji={characterOne.name_kanji} 
-                image_url={characterOne.images.jpg.image_url} about={characterOne.about} />
+                image_url={characterOne.images.jpg.image_url} about={characterOne.about}/>
                 <CharCard name={characterTwo.name} name_kanji={characterTwo.name_kanji} 
-                image_url={characterTwo.images.jpg.image_url} about={characterTwo.about} />
+                image_url={characterTwo.images.jpg.image_url} about={characterTwo.about}/>
             </div> : <div id="starting-characters">
             
             <h1 id="h1-fave">Choose your favorites!</h1>
+            <h4>Add your favorites by clicking on the characters</h4>
             <button className="next-button" id="start-button" onClick={makeQuery}>Start</button>
             </div>}
             {characterOne && characterTwo ? <button className="next-button" onClick={makeQuery}>Next character!</button> : null}
