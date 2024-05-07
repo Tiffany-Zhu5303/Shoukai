b4c982ae5f9e559d4d019f75848773aaed443285
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2024-05-07 11:44:52 -0400

    server and client side

---
+++ server/server.js
@@ -0,0 +1,36 @@
+console.clear();
+
+import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const PORT = 3000;
+
+const server = express();
+
+// Uncomment these as part of Step 3!
+const uri = 'mongodb+srv://Tz02:Hunter695!@atlascluster.s3gh93l.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+await client.connect();
+
+server.get('/connection', async (req, res) => {
+  const ping = await client.db('admin').command({ ping: 1 })
+  const dbs = await client.db().admin().listDatabases();
+  res.end(JSON.stringify({
+    ping,
+    dbs
+  }));
+})
+
+server.get('/db/findOne/:db/:collection', async (req, res) => {
+  const { db, collection } = req.params;
+  const result = await client.db(db).collection(collection).findOne({});
+  res.send(result);
+});
+
+server.get('/', (req, res) => {
+  res.send('<h1>Connected Server!</h1>')
+})
+
+server.listen(PORT, () => {
+  console.log(`Server Listening On Port:${PORT}`);
+})
