75224c7cf40f2a1891d58f048f559284b717d46c
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2024-06-04 18:52:09 -0400

    router basename

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ import CharacterInfo from './pages/CharacterInfo';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/Shoukai'>
       <Routes>
         <Route path='/' element={<Navbar />}>
           <Route index={true} element={<App />}></Route>
