580b5d1edebbf5d133cf653e2de66c0cde97f5ef
Tiffany Zhu <Tiffany2016d@gmail.com>
2025-10-08 16:05:38 -0400

    debug: temp removal

--- client/src/app/lib/mongodb.js
+++ client/src/app/lib/mongodb.js
@@ -1,12 +1,12 @@
-import { MongoClient } from "mongodb";
+// import { MongoClient } from "mongodb";
 
-if (!process.env.MONGODB_URI) throw new Error("Missing MONGODB_URI");
-if (!process.env.MONGODB_PASSWORD) throw new Error("Missing MONGODB_PASSWORD");
+// if (!process.env.MONGODB_URI) throw new Error("Missing MONGODB_URI");
+// if (!process.env.MONGODB_PASSWORD) throw new Error("Missing MONGODB_PASSWORD");
 
-let mongodb_uri = process.env.MONGODB_URI;
-mongodb_uri = mongodb_uri.replace("<db_password>", process.env.MONGODB_PASSWORD);
+// let mongodb_uri = process.env.MONGODB_URI;
+// mongodb_uri = mongodb_uri.replace("<db_password>", process.env.MONGODB_PASSWORD);
 
-const client = new MongoClient(mongodb_uri);
-const db = client.db("Shoukai");
+// const client = new MongoClient(mongodb_uri);
+// const db = client.db("Shoukai");
 
-export { client, db };
+// export { client, db };
