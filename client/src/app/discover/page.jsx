fa514272c8d85ef12c2c91e5bb94e48867f157c1
Tiffany Zhu <Tiffany2016d@gmail.com>
2025-10-18 19:09:36 -0400

    feat: motion added to cards

--- client/src/app/discover/page.jsx
+++ client/src/app/discover/page.jsx
@@ -9,38 +9,43 @@ export default function DiscoverPage() {
             <p className="text-5xl">Discover By</p>
             <div className="p-12 flex space-x-32">
                 <div className="flex flex-col justify-center items-center hover:font-bold">
-                    <Link href="/anime" className="text-center">
-                        <motion.div
-                            whileHover={{ rotate: -25 }}
-                            transition={{ duration: 0.5 }}
-                            className="flex justify-center items-center pb-8"
-                        >
-                            <Image 
-                            className="rounded-lg w-[300px] h-[400px] origin-top-left" 
-                            width={600}
-                            height={600}
-                            src="/DemonSlayerPoster.jpg" 
-                            alt="Demon Slayer Poster" />
-                        </motion.div>
-                    </Link>
-                    <p className="text-center text-2xl pt-4">Anime</p>
+                    <div className="relative inline-block">
+                        <Link href="/anime" className="text-center">
+                            <div className="absolute inset-0 bg-blush rounded-lg shadow-md" />
+                            <motion.div
+                                whileHover={{ rotate: -25 }}
+                                transition={{ duration: 0.5 }}
+                                className="flex justify-center items-center origin-bottom-left"
+                            >
+                                <Image 
+                                className="rounded-lg w-[300px] h-[400px] z-10" 
+                                width={600}
+                                height={600}
+                                src="/DemonSlayerPoster.jpg" 
+                                alt="Demon Slayer Poster" />
+                            </motion.div>
+                        </Link>
+                    </div>
+                    <p className="text-center text-2xl pt-8">Anime</p>
                 </div>
                 <div className="flex flex-col justify-center items-center hover:font-bold">
-                    <Link href="/character" className="text-center">
-                        <motion.div
-                            whileHover={{ rotate: 25 }}
-                            transition={{ duration: 0.5 }}
-                            className="flex justify-center items-center pb-8"
-                        >
-                            <Image 
-                            className="rounded-lg w-[300px] h-[400px] origin-bottom-left" 
-                            width={600}
-                            height={600}
-                            src="/Nezuko.jpg" 
-                            alt="Nezuko" />
-                        </motion.div>
-                    </Link>
-                    <p className="text-center text-2xl pt-4">Character</p>
+                    <div className="relative inline-block">
+                        <Link href="/character" className="text-center">
+                            <div className="absolute inset-0 bg-blush rounded-lg shadow-md" />
+                            <motion.div
+                                whileHover={{ rotate: 25 }}
+                                transition={{ duration: 0.5 }}
+                                className="flex justify-center items-center origin-bottom-right">
+                                <Image 
+                                className="rounded-lg w-[300px] h-[400px] z-10" 
+                                width={600}
+                                height={600}
+                                src="/Nezuko.jpg" 
+                                alt="Nezuko" />
+                            </motion.div>
+                        </Link>
+                    </div>
+                    <p className="text-center text-2xl pt-8">Character</p>
                 </div>
             </div>
         </div>
